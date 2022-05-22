5bbc0f1181803ec46a0ca64c81647e7bb3d53fa8
Fatima <xfatimakx@gmail.com>
2022-05-22 02:02:17 +0000

    Declaring and Calling functions

---
+++ hello.js
@@ -0,0 +1,12 @@
+//const sayHello  = function () {
+  //console.log("Hello, world");}
+
+//sayHello();
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
